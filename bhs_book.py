66f2a4d5c3ebc22da1ee6dacee4825b20ce3724e
Martin Bell <martin@backhand.tech>
2020-08-19 16:02:41 +0100

    adds sql with stories

--- bhs_book.py
+++ bhs_book.py
@@ -1,3 +1,7 @@
 from ebooklib import epub
 
 book = epub.EpubBook()
+book.set_identifier("backhandstories.com-one")
+book.set_title("Backhand Stories: Anthology One")
+book.set_language("en")
+book.add_author("Martin Bell")
