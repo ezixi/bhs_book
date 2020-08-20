241bf9a2257eb0f97bedb36f5f31cfa4fc9abd52
Martin Bell <martin@backhand.tech>
2020-08-20 18:24:03 +0100

    adds db connection

--- bhs_book.py
+++ bhs_book.py
@@ -1,4 +1,6 @@
 from ebooklib import epub
+import psycopg2
+import local_settings
 
 book = epub.EpubBook()
 book.set_identifier("backhandstories.com-one")
@@ -10,3 +12,32 @@ book.add_author("Martin Bell")
 def get_story_ids():
     story_ids = [27]
     return story_ids
+
+
+def connect_to_db():
+    conn = psycopg2.connect(
+        f"dbname={local_settings.LOCALDB} user={local_settings.LOCALUSER}"
+    )
+    return conn
+
+
+def get_story(story_id, connection):
+    cur = connection.cursor()
+    cur.execute(
+        f"""
+        SELECT post_title, post_content from wp_posts where "ID" = {story_id};
+        """
+    )
+    return cur.fetchall()
+
+
+def main():
+    connection = connect_to_db()
+    for story_id in get_story_ids():
+        story = get_story(story_id, connection)
+        print(story)
+    return
+
+
+if __name__ == "__main__":
+    main()
--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,20 @@
 import unittest
 import bhs_book
+import psycopg2
+import local_settings
 
 
 class BookTest(unittest.TestCase):
+    def setUp(self):
+        self.conn = psycopg2.connect(
+            f"dbname={local_settings.LOCALDB} user={local_settings.LOCALUSER}"
+        )
+
     def test_story_ids_exist(self):
         self.assertTrue(len(bhs_book.get_story_ids()) > 0)
+
+    def test_story_has_a_title_and_body(self):
+        self.assertTrue(len(bhs_book.get_story(27, self.conn)) == 2)
 
 
 if __name__ == "__main__":
