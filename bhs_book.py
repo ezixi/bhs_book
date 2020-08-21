81b20aef66e59d86d6acba7629311b4335627389
Martin Bell <martin@backhand.tech>
2020-08-21 13:24:18 +0100

    adds a new BhsBook class

--- bhs_book.py
+++ bhs_book.py
@@ -2,39 +2,40 @@ from ebooklib import epub
 import psycopg2
 import local_settings
 
-book = epub.EpubBook()
-book.set_identifier("backhandstories.com-one")
-book.set_title("Backhand Stories: Anthology One")
-book.set_language("en")
-book.add_author("Martin Bell")
 
+class BhsBook(epub.EpubBook):
+    def __init__(self):
+        book = epub.EpubBook()
+        book.set_identifier("backhandstories.com-one")
+        book.set_title("Backhand Stories: Anthology One")
+        book.set_language("en")
+        book.add_author("Martin Bell")
 
-def get_story_ids():
-    story_ids = [27]
-    return story_ids
+    def get_story_ids(self):
+        story_ids = [27]
+        return story_ids
 
+    def connect_to_db(self):
+        conn = psycopg2.connect(
+            f"dbname={local_settings.LOCALDB} user={local_settings.LOCALUSER}"
+        )
+        return conn
 
-def connect_to_db():
-    conn = psycopg2.connect(
-        f"dbname={local_settings.LOCALDB} user={local_settings.LOCALUSER}"
-    )
-    return conn
-
-
-def get_story(story_id, connection):
-    cur = connection.cursor()
-    cur.execute(
-        f"""
-        SELECT post_title, post_content from wp_posts where "ID" = {story_id};
-        """
-    )
-    return cur.fetchall()
+    def get_story(self, story_id, connection):
+        cur = connection.cursor()
+        cur.execute(
+            f"""
+            SELECT post_title, post_content from wp_posts where "ID" = {story_id};
+            """
+        )
+        return cur.fetchall()
 
 
 def main():
-    connection = connect_to_db()
-    for story_id in get_story_ids():
-        story = get_story(story_id, connection)
+    book = BhsBook()
+    connection = book.connect_to_db()
+    for story_id in book.get_story_ids():
+        story = book.get_story(story_id, connection)
         print(story)
     return
 
