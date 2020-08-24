5ffd97869ea097aefceb73e816b5f45745159681
Martin Bell <martin@backhand.tech>
2020-08-24 19:54:50 +0100

    formatting

--- bhs_book/book.py
+++ bhs_book/book.py
@@ -18,28 +18,6 @@ class BhsBook(epub.EpubBook):
         self.identifier = identifier
         self.language = language
 
-    def connect_to_db(self):
-        conn = psycopg2.connect(
-            f"dbname={local_settings.LOCALDB} user={local_settings.LOCALUSER}"
-        )
-        return conn
-
-    def get_story(self, story_id, connection):
-        cur = connection.cursor()
-        cur.execute(
-            f"""
-            SELECT post_title, post_content from wp_posts where "ID" = {story_id};
-            """
-        )
-        data = cur.fetchall()[0]
-        cur.close()
-        return data
-
-    def clean_story(self, story, replacement_rules):
-        for error, replacement in replacement_rules.items():
-            story = re.sub(error, replacement, story)
-        return story
-
     def create_folder(self):
         folder_name = self.title.replace(" ", "-")
         distutils.dir_util.mkpath(f"/tmp/{folder_name}")
