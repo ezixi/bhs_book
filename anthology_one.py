55b6d43e031102de90705da8784d7935b4192b28
Martin Bell <martin@backhand.tech>
2020-08-24 19:34:01 +0100

    collates paths

--- anthology_one.py
+++ anthology_one.py
@@ -16,11 +16,15 @@ def main():
     }
     story_ids = [27, 45]
     connection = new_book.connect_to_db()
+    paths = []
     for story_id in story_ids:
         data = new_book.get_story(story_id, connection)
         cleaned_story = new_book.clean_story(data[1], replacement_rules)
-        new_book.write_html(data[0], cleaned_story, story_ids.index(story_id))
+        paths.append(
+            new_book.write_html(data[0], cleaned_story, story_ids.index(story_id))
+        )
     connection.close()
+    new_book.collate_chapters(paths)
     return
 
 
